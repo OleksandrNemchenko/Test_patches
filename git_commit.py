a8ccdafd0b047089cbdc8071dd887cc7a2eb310a
Barbeque Meal <petr125g@gmail.com>
2025-10-14 16:19:22 +0300

    Some feature 5

--- git_commit.py
+++ git_commit.py
@@ -8,6 +8,7 @@ from loguru import logger
 from pathlib import Path
 
 args = None
+projects = ['Test_Big1', 'Test_Big2']
 
 def execute_cmd(command):
     command_line = " ".join(command)
@@ -35,15 +36,31 @@ def execute_cmd(command):
 
     return output, process.returncode
 
+def make_commit(git_commands, project_name: str = None):
+    if project_name:
+        logger.info(f'Make commit "{args.message}" for {project_name}')
+    else:
+        logger.info(f'Make commit "{args.message}" for the whole repository')
+
+    cmd = [f'git']
+    if project_name:
+        cmd.extend(['-C', str(args.path / project_name)])
+    cmd.append('commit')
+    cmd.extend(git_commands)
+
+    _, returncode = execute_cmd(cmd)
+    if not returncode == 0:
+        raise Exception(f'Unable to make commit')
+
 def make_patches(project_name: str):
     upstream = ""
-    if args.u == "":
+    if args.upstream == "":
         logger.info(f'Get upstream branch for {project_name} project')
         output, _ = execute_cmd([f'git -C {str(args.path / project_name)} rev-parse --abbrev-ref --symbolic-full-name @{{u}}'])
         upstream = output[0]
         logger.success(f'Upstream for {project_name}: {upstream}')
     else:
-        upstream = args.u
+        upstream = args.upstream
 
     logger.info(f'Make patches for {project_name} for {upstream} upstream')
     execute_cmd([f'git -C {str(args.path / project_name)} format-patch {upstream} -o {str(args.path / "overlay" / f"{project_name}_patches")}'])
@@ -51,15 +68,20 @@ def make_patches(project_name: str):
 def main():
     global args
 
-    parser = argparse.ArgumentParser(description = "Make patches")
-    parser.add_argument("path", type = str, help = "The directory with the project")
-    parser.add_argument("-u", type = str, required = False, default = "", help = "upstream branch for sub projects; if empty, current upstream branch will be used")
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description = "Immitage git command for all subprojects and make patches")
+    parser.add_argument("--path", type = str, required = False, default = os.getcwd(), help = "The directory with the project")
+    parser.add_argument("-m", "--message", type = str, required = True, help = "git message for subprojects")
+    parser.add_argument("--upstream", type = str, required = False, default = "", help = "upstream branch for sub projects; if empty, current upstream branch will be used")
+    args, unknown_args = parser.parse_known_args()
     args.path = Path(args.path)
 
-    make_patches('Test_Big1')
-    make_patches('Test_Big2')
+    git_commands = unknown_args.copy()
+    if args.message and '-m' not in unknown_args and '--message' not in unknown_args:
+        git_commands.extend(['-m', f'"{args.message}"'])
 
+    [make_commit(git_commands, project) for project in projects]
+    [make_patches(project) for project in projects]
+    make_commit(git_commands)
 
 if __name__ == "__main__":
 
