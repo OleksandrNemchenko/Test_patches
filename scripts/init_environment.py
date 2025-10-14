404712f27351cf1cd8ec320315519fba7bfcb6cc
Barbeque Meal <petr125g@gmail.com>
2025-10-14 15:21:46 +0300

    Some tests

---
+++ scripts/init_environment.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+import traceback
+from loguru import logger
+from pathlib import Path
+
+args = None
+
+def execute_cmd(command):
+    logger.debug(" ".join(command))
+
+    process = subprocess.Popen(
+        command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+        text = True,
+        bufsize = 1,
+        shell = True
+    )
+
+    for line in process.stdout:
+        print(line, end = '')
+
+    process.wait()
+
+    if process.returncode == 0:
+        logger.success(f'Return code: {process.returncode}')
+    else:
+        logger.error(f'Return code: {process.returncode}')
+
+def init_test_big1():
+    logger.info('Init subproject Test_Big1')
+    execute_cmd(['git', 'clone', 'https://github.com/OleksandrNemchenko/Test_Big1.git', str(args.path / "Test_Big1")])
+
+def init_test_big2():
+    logger.info('Init subproject Test_Big2')
+    execute_cmd(['git', 'clone', '--recurse-submodules', 'https://github.com/OleksandrNemchenko/Test_Big2.git', str(args.path / "Test_Big2")])
+
+def main():
+    global args
+
+    parser = argparse.ArgumentParser(description = "Init environment")
+    parser.add_argument("--path", type = str, required = False, default = os.getcwd(), help = "The directory with the project")
+    args = parser.parse_args()
+    args.path = Path(args.path)
+
+    logger.info(f'Start environment initialization')
+
+    if Path("Test_Big1").exists():
+        logger.debug(f'Project "Test_Big1" exists')
+    else:
+        init_test_big1()
+
+    if Path("Test_Big2").exists():
+        logger.debug(f'Project "Test_Big2" exists')
+    else:
+        init_test_big2()
+
+if __name__ == "__main__":
+
+    try:
+        main()
+
+    except Exception as exception:
+
+        logger.critical(f"*** {exception}")
+
+        logger.debug("Traceback")
+        traceback.print_exc()
---
+++ scripts/make_patches.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+import traceback
+from loguru import logger
+from pathlib import Path
+
+args = None
+
+def execute_cmd(command):
+    command_line = " ".join(command)
+    logger.debug(command_line)
+
+    process = subprocess.Popen(
+        command_line,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+        text = True,
+        bufsize = 1,
+        shell = True
+    )
+
+    output = []
+    for line in process.stdout:
+        line_without_end = line.rstrip('\n')
+        print(line_without_end)
+        output.append(line_without_end)
+
+    process.wait()
+
+    if not process.returncode == 0:
+        logger.error(f'Return code: {process.returncode}')
+
+    return output, process.returncode
+
+def make_patches(project_name: str):
+    upstream = ""
+    if args.upstream == "":
+        logger.info(f'Get upstream branch for {project_name} project')
+        output, _ = execute_cmd([f'git -C {str(args.path / project_name)} rev-parse --abbrev-ref --symbolic-full-name @{{u}}'])
+        upstream = output[0]
+        logger.success(f'Upstream for {project_name}: {upstream}')
+    else:
+        upstream = args.upstream
+
+    logger.info(f'Make patches for {project_name} for {upstream} upstream')
+    execute_cmd([f'git -C {str(args.path / project_name)} format-patch {upstream} -o {str(args.path / "overlay" / f"{project_name}_patches")}'])
+
+def main():
+    global args
+
+    parser = argparse.ArgumentParser(description = "Make patches")
+    parser.add_argument("--path", type = str, required = False, default = os.getcwd(), help = "The directory with the project")
+    parser.add_argument("--upstream", type = str, required = False, default = "", help = "upstream branch for sub projects; if empty, current upstream branch will be used")
+    args = parser.parse_args()
+    args.path = Path(args.path)
+
+    make_patches('Test_Big1')
+    make_patches('Test_Big2')
+
+
+if __name__ == "__main__":
+
+    try:
+        main()
+
+    except Exception as exception:
+
+        logger.critical(f"*** {exception}")
+
+        logger.debug("Traceback")
+        traceback.print_exc()
