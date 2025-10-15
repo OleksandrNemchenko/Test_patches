f6956ea2ad4767df135622a84c481f21302820f1
Barbeque Meal <petr125g@gmail.com>
2025-10-15 18:35:50 +0300

    Patches and other staff

--- git_pull.py
+++ git_pull.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 import subprocess
 import traceback
@@ -43,7 +44,7 @@ def git_pull():
     if not result == 0:
         raise Exception (f'Error while executing command')
 
-def use_patches(project_name: str):
+def use_patches(project_name: str, applied_patches: dict, applied_patches_path: str):
     upstream = ""
     if args.upstream == "":
         logger.info(f'Get upstream branch for {project_name} project')
@@ -59,8 +60,35 @@ def use_patches(project_name: str):
     patch_files = sorted(f for f in os.listdir(patch_dir) if f.endswith(".patch"))
     patch_paths = [os.path.join(patch_dir, f) for f in patch_files]
 
+    applied_patches_for_project = applied_patches[project_name]
+    begin_patches_processing = applied_patches_for_project == ""
+
     for patch in patch_paths:
+        if not begin_patches_processing:
+            if patch == applied_patches_for_project:
+                begin_patches_processing = True
+            continue
+
         execute_cmd([f'git -C {str(args.path / project_name)} am --3way {patch}'])
+        applied_patches[project_name] = patch
+
+    with open(applied_patches_path, 'w') as file:
+        json.dump(applied_patches, file, indent = 4)
+
+def load_applied_patches(file_path):
+
+    if not file_path.exists():
+        logger.info(f'Applied patches file {file_path} does not exist')
+        json_file = {}
+        for project in projects:
+            json_file[project] = ""
+        return json_file
+
+    with open(file_path, 'r') as file:
+        json_file = json.load(file)
+
+        logger.success(f'Applied patches file {file_path} was read')
+        return json_file
 
 def main():
     global args
@@ -72,7 +100,10 @@ def main():
     args.path = Path(args.path)
 
     git_pull()
-    [use_patches(project) for project in projects]
+
+    applied_patches_path = Path(args.path / "overlay" / "applied_patches.json")
+    applied_patches = load_applied_patches(applied_patches_path)
+    [use_patches(project, applied_patches, applied_patches_path) for project in projects]
 
 if __name__ == "__main__":
 
